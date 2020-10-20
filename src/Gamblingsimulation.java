c3528b9a378683951f029c77ee3c482712e5f7f3
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-20 15:46:01 +0530

    added winloose method to check gambler is win or Loose

--- src/Gamblingsimulation.java
+++ src/Gamblingsimulation.java
@@ -1,5 +1,20 @@
+
+
 public class Gamblingsimulation {
+    public static final int STAKE=100;
+    public static final int BET=1;
+
     public static void main(String[] args) {
         System.out.println("welcome to gambling game");
+        Gamblingsimulation gamblingsimulation=new Gamblingsimulation();
+        gamblingsimulation.winLoose();
+    }
+    public void winLoose(){
+        double randomCheck=Math.floor(Math.random() * 10) % 2;
+        if((int)randomCheck==1){
+            System.out.println("gambler win ");
+        } else{
+            System.out.println("gambler loose ");
+        }
     }
 }
