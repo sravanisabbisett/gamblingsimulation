9b3b37f4fbe448cf38bf914fb8bbc6754544fd30
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-20 16:00:44 +0530

    modified winLoose method to cal 50 percent winLoose

--- src/Gamblingsimulation.java
+++ src/Gamblingsimulation.java
@@ -3,6 +3,7 @@
 public class Gamblingsimulation {
     public static final int STAKE=100;
     public static final int BET=1;
+    int temp=STAKE;
 
     public static void main(String[] args) {
         System.out.println("welcome to gambling game");
@@ -10,11 +11,17 @@ public class Gamblingsimulation {
         gamblingsimulation.winLoose();
     }
     public void winLoose(){
-        double randomCheck=Math.floor(Math.random() * 10) % 2;
-        if((int)randomCheck==1){
-            System.out.println("gambler win ");
-        } else{
-            System.out.println("gambler loose ");
+        while (true) {
+            double randomCheck=Math.floor(Math.random() * 10) % 2;
+            if ((int) randomCheck == 1) {
+                temp++;
+            } else {
+                temp--;
+            }
+            if(temp==50||temp==150){
+                System.out.println(temp);
+                break;
+            }
         }
     }
 }
