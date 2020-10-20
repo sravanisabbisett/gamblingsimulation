10e2585efa390cbe2ec827b030867e6f8ee02be3
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-20 17:12:07 +0530

    refactored code

--- src/Gamblingsimulation.java
+++ src/Gamblingsimulation.java
@@ -1,27 +1,39 @@
 
 
-public class Gamblingsimulation {
+public class GamblingSimulation {
     public static final int STAKE=100;
     public static final int BET=1;
-    int temp=STAKE;
+    int tempMoney=STAKE;
 
     public static void main(String[] args) {
         System.out.println("welcome to gambling game");
-        Gamblingsimulation gamblingsimulation=new Gamblingsimulation();
-        gamblingsimulation.winLoose();
+        GamblingSimulation gamblingSimulation=new GamblingSimulation();
+        gamblingSimulation.gamble();
     }
-    public void winLoose(){
-        while (true) {
-            double randomCheck=Math.floor(Math.random() * 10) % 2;
-            if ((int) randomCheck == 1) {
-                temp++;
-            } else {
-                temp--;
-            }
-            if(temp==50||temp==150){
-                System.out.println(temp);
-                break;
-            }
+    public boolean winLoose() {
+        double randomCheck=Math.floor(Math.random() * 10) % 2;
+        if((int)randomCheck==1) {
+            System.out.println("win");
+            return true;
+        }
+        else {
+            System.out.println("Loose");
+            return false;
         }
     }
+    public void gamble(){
+        int tempMoney=STAKE;
+        while (tempMoney > 50 && tempMoney < 150) {
+            if (winLoose()) {
+                tempMoney=tempMoney+BET;
+                System.out.println("Money after gamble"+tempMoney);
+            } else {
+                tempMoney=tempMoney-BET;
+                System.out.println("Money after gamble"+tempMoney);
+            }
+        }
+        System.out.println("Resigned for day"+tempMoney);
+    }
 }
+
+
